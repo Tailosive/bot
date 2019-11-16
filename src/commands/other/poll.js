eb229cb54e8b49ceb4edfb85522dbe9dcd064004
Adam <adam@atiktech.co>
2019-11-15 21:47:39 -0600

    :art: poll bug

--- src/commands/other/poll.js
+++ src/commands/other/poll.js
@@ -15,7 +15,7 @@ class Poll extends Command {
     if (!args.join()) return msg.embed('Please give a question to ask for a poll!')
     const embed = this.client.embed()
       .title(`Poll by ${this.client.functions.escapeRegex(`${msg.author.username}#${msg.author.discriminator}`)}`)
-      .description(args.join())
+      .description(args.join(' '))
       .timestamp(new Date())
       .color(await msg.color())
       .footer(await msg.text(), await msg.logo())
