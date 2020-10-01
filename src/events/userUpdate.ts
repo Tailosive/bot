4f9ad46e2e4879170514f16d38d2e37481422be1
Adam <adam@atiktech.co>
2020-10-01 17:20:08 -0500

    :bug: added try catch idk why its complaining

--- src/events/userUpdate.ts
+++ src/events/userUpdate.ts
@@ -19,60 +19,64 @@ class UserUpdateListener extends Event {
       avatar?: string
     }
   ) {
-    if ((user?.username || '') !== (oldUser?.username || '')) {
-      const embed = new Embed()
-        .setTitle('**Username Changed**')
-        .addField(
-          'Member',
-          `${user.mention} \`${user?.username || ''}#${user.discriminator}\``,
-          false
+    try {
+      if ((user?.username || '') !== (oldUser?.username || '')) {
+        const embed = new Embed()
+          .setTitle('**Username Changed**')
+          .addField(
+            'Member',
+            `${user.mention} \`${user?.username || ''}#${user.discriminator}\``,
+            false
+          )
+          .addField('Before', `\`${oldUser?.username || ''}\``, true)
+          .addField('After', `\`${user?.username || ''}\``, true)
+          .setTimestamp(new Date())
+          .setThumbnail(user.avatarURL || user.defaultAvatarURL)
+          .setColor(0xff00ff)
+          .setFooter(
+            `ID: ${user.id} | ${this.client.config.embed.text}`,
+            this.client.config.embed.icon
+          )
+        return this.client.createMessage(
+          this.client.config.channels.nickname_channel,
+          { embed: embed }
         )
-        .addField('Before', `\`${oldUser?.username || ''}\``, true)
-        .addField('After', `\`${user?.username || ''}\``, true)
-        .setTimestamp(new Date())
-        .setThumbnail(user.avatarURL || user.defaultAvatarURL)
-        .setColor(0xff00ff)
-        .setFooter(
-          `ID: ${user.id} | ${this.client.config.embed.text}`,
-          this.client.config.embed.icon
+      } else if ((user?.avatar || '') !== (oldUser?.avatar || '')) {
+        const embed = new Embed()
+          .setTitle('**Avatar Changed**')
+          .addField(
+            'Member',
+            `${user?.mention} \`${user?.username}#${user?.discriminator}\``,
+            false
+          )
+          .addField(
+            'Old Avatar',
+            `[View Old Avatar](${
+              user.avatar
+                ? `https://cdn.discordapp.com/avatars/${user.id}/${oldUser.avatar}.png`
+                : `https://cdn.discordapp.com/embed/avatars/${oldUser.discriminator}.png`
+            })`,
+            true
+          )
+          .addField(
+            'New Avatar',
+            `[View New Avatar](${user.avatarURL || user.defaultAvatarURL})`,
+            true
+          )
+          .setTimestamp(new Date())
+          .setImage(user.avatarURL || user.defaultAvatarURL)
+          .setColor(0xff00ff)
+          .setFooter(
+            `ID: ${user.id} | ${this.client.config.embed.text}`,
+            this.client.config.embed.icon
+          )
+        return this.client.createMessage(
+          this.client.config.channels.nickname_channel,
+          { embed: embed }
         )
-      return this.client.createMessage(
-        this.client.config.channels.nickname_channel,
-        { embed: embed }
-      )
-    } else if ((user?.avatar || '') !== (oldUser?.avatar || '')) {
-      const embed = new Embed()
-        .setTitle('**Avatar Changed**')
-        .addField(
-          'Member',
-          `${user?.mention} \`${user?.username}#${user?.discriminator}\``,
-          false
-        )
-        .addField(
-          'Old Avatar',
-          `[View Old Avatar](${
-            user.avatar
-              ? `https://cdn.discordapp.com/avatars/${user.id}/${oldUser.avatar}.png`
-              : `https://cdn.discordapp.com/embed/avatars/${oldUser.discriminator}.png`
-          })`,
-          true
-        )
-        .addField(
-          'New Avatar',
-          `[View New Avatar](${user.avatarURL || user.defaultAvatarURL})`,
-          true
-        )
-        .setTimestamp(new Date())
-        .setImage(user.avatarURL || user.defaultAvatarURL)
-        .setColor(0xff00ff)
-        .setFooter(
-          `ID: ${user.id} | ${this.client.config.embed.text}`,
-          this.client.config.embed.icon
-        )
-      return this.client.createMessage(
-        this.client.config.channels.nickname_channel,
-        { embed: embed }
-      )
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 }
